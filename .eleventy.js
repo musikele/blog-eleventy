339a6d772a7f091edab3b8b45a5e5ce4f4178401
Michele Nasti <michele@michelenasti.com>
2022-02-13 23:56:36 +0100

    use simple.css. close #1

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,22 @@
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
     // Copy the `img` and `css` folders to the output
     eleventyConfig.addPassthroughCopy('images');
+    eleventyConfig.addPassthroughCopy('uploads');
     eleventyConfig.addPassthroughCopy('css');
 
     // Add plugins
     eleventyConfig.addPlugin(pluginRss);
     eleventyConfig.addPlugin(pluginSyntaxHighlight);
+
+    eleventyConfig.addFilter('postDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj)
+            .setLocale('en')
+            .toLocaleString(DateTime.DATE_FULL);
+    });
 
     return {
         // Control which files Eleventy will process
