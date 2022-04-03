e3ca2c7a0e3c89ff14e9a2a3290a9bc480cd0c30
Michele Nasti <michele@rtk.io>
2022-04-03 22:03:13 +0200

    tags

--- .eleventy.js
+++ .eleventy.js
@@ -35,6 +35,16 @@ module.exports = function (eleventyConfig) {
         return DateTime.fromJSDate(dateObj).toFormat("yyyy/MM/dd");
     });
 
+    eleventyConfig.addFilter('sortObjectByKey', (collection) => {
+        const entries = Object.entries(collection);
+        const toReturn = entries.sort((entry1, entry2) => {
+            if (entry1[0] <= entry2[0]) return -1;
+            else return 1;
+
+        });
+        return toReturn;
+    });
+
     // Creates the extension for use
     eleventyConfig.addExtension("scss", {
         outputFileExtension: "css", // optional, default: "html"
